c4d38a1028417dfd288d8507969120960c8821e1
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-04-09 11:00:06 +0100

    build from lecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,52 @@
 import React from 'react';
 
 class App extends React.Component {
+  //RENDER IS NOT BEING CALLED IT'S BEING DEFINED
+  state = {
+    todos: [{text: 'Water plants', completed: false}, {text:'Got to gym', completed:false}, {text:'Feed python', completed:false}, {text:'Learn Python', completed:false}]
+  };
   render () {
+    const {todo} = this.state
     return (
       <div>
-        <h1>Welcome to React</h1>
-        <p>Start working from <code>/src/components/App.js</code></p>
+        <Heading name="Jonny"/>
+        <Checklist 
+        todos={this.state.todos}
+        removeTodo={this.removeTodo}/>
       </div>
     );
   }
+  removeTodo = (taskToRemove) => {
+    const newTodos = this.state.todos.filter(task => {
+      return task !== taskToRemove;
+    })
+    this.setState({
+      todos: newTodos
+    });
+  }
+}
+
+function Checklist({todos, removeTodo}) {
+  return <ul>
+    {todos.map((task, i) => {
+      return <Task 
+        task={task}
+        key={`${i}${task}`}
+        removeTodo={removeTodo}
+      />
+    })}
+  </ul>
+}
+
+function Task({task, removeTodo}) {
+  console.log('removing todo')
+  return <li onClick={() => removeTodo(task)}>{task.text}</li>
+}
+
+//FUNCTION HAS TO RETURN A REACT ELEMENT/JSX
+//Caps on Heading because it's a class
+function Heading ({name}) { //destructuring props
+  return <h1>{name}'s Todos</h1>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-import App from './components/App'
+//THIS P IS JSX
+// const helloWorld = <p>Hello World</p>; //THIS IS DECLARATIVE
+// const helloWorld = React.createElement('p', null, 'Hello World'); //THIS IS IMPERATIVE
+//is transpiled into javascript
 
+const helloWorld = <p> 2 + 2 = {2+3}</p>;
+const yinyang = (
+    <div>
+        <p>yin</p>
+        <p>yang</p>
+    </div>
+);
+console.log(yinyang)
 ReactDOM.render(<App />, document.getElementById('root'));
