ed99d919f1b11e627861abf9e1b74fc3dfae11fe
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-04-09 11:54:23 +0100

    line through added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 class App extends React.Component {
   //RENDER IS NOT BEING CALLED IT'S BEING DEFINED
   state = {
-    todos: [{text: 'Water plants', completed: false}, {text:'Got to gym', completed:false}, {text:'Feed python', completed:false}, {text:'Learn Python', completed:false}]
+    todos: [{text: 'Water plants', completed: false}, {text:'Got to gym', completed:false}, {text:'Feed python', completed:false}, {text:'Learn Python', completed:false}],
   };
   render () {
     const {todo} = this.state
@@ -12,7 +13,8 @@ class App extends React.Component {
         <Heading name="Jonny"/>
         <Checklist 
         todos={this.state.todos}
-        removeTodo={this.removeTodo}/>
+        removeTodo={this.removeTodo}
+        toggleComplete={this.toggleComplete}/>
       </div>
     );
   }
@@ -24,24 +26,45 @@ class App extends React.Component {
       todos: newTodos
     });
   }
+  toggleComplete = (taskTotoggleComplete) => {
+    const newTodos = this.state.todos.map(task => {
+      return task !== taskTotoggleComplete ? task : 
+      task.completed === true ? {text: task.text, completed: false} :
+      {text: task.text, completed: true};
+    })
+    this.setState({
+      todos: newTodos
+    })
+  }
 }
 
-function Checklist({todos, removeTodo}) {
+function Checklist({todos, removeTodo, toggleComplete}) {
   return <ul>
     {todos.map((task, i) => {
       return <Task 
         task={task}
         key={`${i}${task}`}
         removeTodo={removeTodo}
+        toggleComplete={toggleComplete}
       />
     })}
   </ul>
 }
 
-function Task({task, removeTodo}) {
-  console.log('removing todo')
-  return <li onClick={() => removeTodo(task)}>{task.text}</li>
+// function Task({task, removeTodo}) {
+//   console.log('removing todo')
+//   return <li onClick={() => removeTodo(task)}>{task.text}</li>
+// }
+function Task({task, toggleComplete}) {
+  console.log('crossing through')
+  console.log(task)
+  const line = {
+    "text-decoration": "line-through"
+  }
+  return task.completed ? <li style={line} onClick={() => toggleComplete(task)}>{task.text}</li>:
+   <li onClick={() => toggleComplete(task)}>{task.text}</li>;
 }
+
 
 //FUNCTION HAS TO RETURN A REACT ELEMENT/JSX
 //Caps on Heading because it's a class
